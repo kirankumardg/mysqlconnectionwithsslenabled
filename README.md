d767c63a1f232681c556c09b59ce69c4109c5202
kdg <kirankumar_dg@intuit.com>
2019-12-18 20:17:17 +0530

    new files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mysqlconnectionwithsslenabled
+	Code which uses ssl to connect to mysql db
+
+## Author
+a. Name: kiran D G
+b. Email: kirankumardg@gmail.com
---
+++ src/main/java/com/cloudcontrolled/sample/mysql/App.java
@@ -0,0 +1,43 @@
+package com.cloudcontrolled.sample.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.net.ssl.SSLContext;
+
+public class App {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("hello comint to main atleast");
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");			
+			SSLContext sc = SSLContext.getInstance("TLSv1");
+			Connection con = getConnection();
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from COMPANY_ROLES limit 2");
+			System.out.println(rs.getFetchSize());
+			while(rs.next()) {
+				System.out.println(rs.getString(1));
+			}
+			
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	protected static Connection getConnection() throws SQLException {
+		String host = "roleservice-accessdb-prd-clus.cluster-c9pfxhs9kqbq.us-west-2.rds.amazonaws.com";
+		String port = "3306";
+		String database = "roleManagementDB ";
+		String username = "metadata_user";
+		String password = "Datalake@123";
+		String dbUrl = "jdbc:mysql://" + host + ":" + port + "/" + database	+"?verifyServerCertificate=true&useSSL=true&requireSSL=true&enabledTLSProtocols=TLSv1.2";
+		return DriverManager.getConnection(dbUrl, username, password);
+	}
+}
